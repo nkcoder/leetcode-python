514525d405e71cc84b1e28275c38b3a78528ed7b
daniel.guo <daniel.guo@vivalabs.com.au>
2024-10-27 21:28:59 +1100

    two_sum: 3rd solution

--- src/array_hash/two_sum.py
+++ src/array_hash/two_sum.py
@@ -1,20 +1,32 @@
 # Easy: https:// leetcode.com/problems/two-sum/
 
 
-# nums map, search the complement
-def two_sum(nums: list[int], target: int) -> list[int]:
+# search the complement => build the nums map
+def two_sum(nums: list[int], target: int) -> bool:
+    prev_map = {}  # value: index
+    for i, v in enumerate(nums):
+        complement = target - v
+        if complement in prev_map:
+            return [prev_map[complement], i]
+        prev_map[v] = i
+
+    return
+
+
+# initialize nums map => search the complement
+def two_sum2(nums: list[int], target: int) -> list[int]:
     nums_map = {nums[i]: i for i in range(len(nums))}
 
     for i in range(len(nums)):
         complement = target - nums[i]
         if complement in nums_map and nums_map[complement] != i:
-            return list([i, nums_map[complement]])
+            return [i, nums_map[complement]]
 
-    return list([-1, -1])
+    return [-1, -1]
 
 
-# nums map => sort
-def two_sum2(nums: list[int], target: int) -> list[int]:
+# initialize nums map => sort => search
+def two_sum3(nums: list[int], target: int) -> list[int]:
     # value => index, the index is a set, because we might the same element twice in the input, for example: [3, 3]
     nums_map: dict[int, set[int]] = {}
 
--- src/array_hash/two_sum_test.py
+++ src/array_hash/two_sum_test.py
@@ -1,4 +1,4 @@
-from two_sum import two_sum, two_sum2
+from two_sum import two_sum, two_sum2, two_sum3
 
 
 def test_two_sum():
@@ -10,3 +10,9 @@ def test_two_sum2():
     assert two_sum2([2, 7, 11, 15], 9) == [0, 1]
     assert two_sum2([3, 2, 4], 6) == [1, 2]
     assert two_sum2([3, 3], 6) == [0, 1]
+
+def test_two_sum3():
+    assert two_sum3([2, 7, 11, 15], 9) == [0, 1]
+    assert two_sum3([3, 2, 4], 6) == [1, 2]
+    assert two_sum3([3, 3], 6) == [0, 1]
+    
