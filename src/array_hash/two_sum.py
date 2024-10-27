afa7ff5ed083f515f254fa582264a817e33a00f4
daniel.guo <daniel.guo@vivalabs.com.au>
2024-10-27 21:03:51 +1100

    two_sum: 2nd solution

--- src/array_hash/two_sum.py
+++ src/array_hash/two_sum.py
@@ -1,14 +1,15 @@
 # Easy: https:// leetcode.com/problems/two-sum/
 
+# nums map => sort
 def two_sum(nums: list[int], target: int) -> list[int]:
-    # the value is a set, because we might the same element twice in the input, for example: [3, 3]
-    index_map: dict[int, set[int]] = {}
+    # value => index, the index is a set, because we might the same element twice in the input, for example: [3, 3]
+    nums_map: dict[int, set[int]] = {}
 
     for i in range(len(nums)):
-        if nums[i] in index_map:
-            index_map.get(nums[i]).add(i)
+        if nums[i] in nums_map:
+            nums_map.get(nums[i]).add(i)
         else:
-            index_map[nums[i]] = {i}
+            nums_map[nums[i]] = {i}
 
     nums.sort()
     i, j = 0, len(nums) - 1
@@ -16,11 +17,23 @@ def two_sum(nums: list[int], target: int) -> list[int]:
     while i <= j:
         tmp_sum = nums[i] + nums[j]
         if tmp_sum == target:
-            if index_map[nums[i]] == index_map[nums[j]]:
-                return list(index_map[nums[i]])
-            return list([index_map[nums[i]].pop(), index_map[nums[j]].pop()])
+            if nums_map[nums[i]] == nums_map[nums[j]]:
+                return list(nums_map[nums[i]])
+            return list([nums_map[nums[i]].pop(), nums_map[nums[j]].pop()])
         elif tmp_sum > target:
             j -= 1
         else:
             i += 1
 
+# nums map, search the complement
+def two_sum2(nums: list[int], target: int) -> list[int]:
+    nums_map: dict[int, int] = {}
+    for i in range(len(nums)):
+        nums_map[nums[i]] = i
+
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in nums_map and nums_map[complement] != i:
+            return list([i, nums_map[complement]])
+
+    return list([-1, -1])
--- src/array_hash/two_sum_test.py
+++ src/array_hash/two_sum_test.py
@@ -1,7 +1,12 @@
-from two_sum import two_sum
+from two_sum import two_sum, two_sum2
 
 
 def test_two_sum():
     assert two_sum([2, 7, 11, 15], 9) == [0, 1]
     assert two_sum([3, 2, 4], 6) == [1, 2]
     assert two_sum([3, 3], 6) == [0, 1]
+
+def test_two_sum2():
+    assert two_sum2([2, 7, 11, 15], 9) == [0, 1]
+    assert two_sum2([3, 2, 4], 6) == [1, 2]
+    assert two_sum2([3, 3], 6) == [0, 1]
