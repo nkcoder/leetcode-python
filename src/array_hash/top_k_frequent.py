870a6cae8e54f4ab3d59c4f4ef2bd2eeb9b21691
daniel.guo <daniel.guo@vivalabs.com.au>
2024-10-28 18:23:07 +1100

    top_k_frequent: O(n)

--- src/array_hash/top_k_frequent.py
+++ src/array_hash/top_k_frequent.py
@@ -1,27 +1,46 @@
 # Medium: https://leetcode.com/problems/top-k-frequent-elements/description/
 
 
-# use hash for count => sort
-# complexity: T-O(nlogn), S-O(n)
+# use hash for count => use list for reversed count, no need to sort
+# similar to bucket sort
+# complexity: T-O(n), S-O(n)
+from collections import defaultdict
 def top_k_frequent(nums: list[int], k: int) -> list[int]:
-    num_2_count: dict[int, int] = {}
+    num_count: dict[int, int] = {}
+    for n in nums:
+        num_count[n] = num_count.get(n, 0) + 1
+
+    frequency: list[list] = [[] for i in range(len(nums) + 1)]
+    for n, c in num_count.items():
+        frequency[c].append(n)
+
+    result = []
+    for i in range(len(frequency) - 1, 0, -1):
+        print(f"i = {i}, f.len = {len(frequency)}")
+        if frequency[i]:
+            result.extend(frequency[i])
+        if len(result) == k:
+            return result
+
+
+# use hash for count => reverse count => sort
+# complexity: T-O(nlogn), S-O(n)
+def top_k_frequent2(nums: list[int], k: int) -> list[int]:
+    num_count: dict[int, int] = {}
 
     for n in nums:
-        num_2_count[n] = num_2_count.get(n, 0) + 1
+        num_count[n] = num_count.get(n, 0) + 1
 
-    count_2_num: dict[int, list] = {}
-    for n, c in num_2_count.items():
-        if c in count_2_num:
-            count_2_num[c].append(n)
-        else:
-            count_2_num[c] = [n]
+    num_count_reversed: dict[int, list] = defaultdict(list)
+    for n, c in num_count.items():
+        num_count_reversed[c].append(n)
 
-    count_sorted = sorted(list(count_2_num.keys()), reverse=True)
+    count_sorted = sorted(list(num_count_reversed.keys()), reverse=True)
 
     result = []
     i = 0
     while len(result) < k and i < len(count_sorted):
-        data = count_2_num[count_sorted[i]]
+        data = num_count_reversed[count_sorted[i]]
         result.extend(data)
         i += 1
 
--- src/array_hash/top_k_frequent_test.py
+++ src/array_hash/top_k_frequent_test.py
@@ -1,5 +1,4 @@
-from top_k_frequent import top_k_frequent
-
+from top_k_frequent import top_k_frequent2, top_k_frequent
 
 def test_top_k_frequent():
     assert top_k_frequent([1, 1, 1, 2, 2, 3], 2) == [1, 2]
@@ -7,6 +6,47 @@ def test_top_k_frequent():
     assert top_k_frequent([1, 2], 2) == [1, 2]
 
     actual = top_k_frequent(
+        [
+            3,
+            2,
+            3,
+            1,
+            2,
+            4,
+            5,
+            5,
+            6,
+            7,
+            7,
+            8,
+            2,
+            3,
+            1,
+            1,
+            1,
+            10,
+            11,
+            5,
+            6,
+            2,
+            4,
+            7,
+            8,
+            5,
+            6,
+        ],
+        10,
+    )
+    expected = [1, 2, 5, 3, 6, 7, 4, 8, 10, 11]
+    assert sorted(actual) == sorted(expected)
+
+
+def test_top_k_frequent2():
+    assert top_k_frequent2([1, 1, 1, 2, 2, 3], 2) == [1, 2]
+    assert top_k_frequent2([1], 1) == [1]
+    assert top_k_frequent2([1, 2], 2) == [1, 2]
+
+    actual = top_k_frequent2(
         [
             3,
             2,
