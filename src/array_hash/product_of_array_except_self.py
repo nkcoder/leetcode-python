805f333713b950235848f1931b05f3b1b952714a
daniel.guo <daniel.guo@vivalabs.com.au>
2024-11-03 18:16:14 +1100

    product of array except self: S-O(1)

--- src/array_hash/product_of_array_except_self.py
+++ src/array_hash/product_of_array_except_self.py
@@ -1,13 +1,36 @@
+# Medium: https://leetcode.com/problems/product-of-array-except-self/description/
+
+
+# Calculate the left product and right product for each index and save to an array
+# Complexity: T-O(n), S-O(n)
 def product_of_array_except_self(nums: list[int]) -> list[int]:
     # calculate the left products
     left_products = [1] * len(nums)
     for i in range(1, len(nums)):
-        left_products[i] = left_products[i-1] * nums[i-1]
+        left_products[i] = left_products[i - 1] * nums[i - 1]
 
     # calculate the right products
     right_products = [1] * len(nums)
     for i in range(len(nums) - 2, -1, -1):
-        right_products[i] = right_products[i + 1] * nums[i+1]
+        right_products[i] = right_products[i + 1] * nums[i + 1]
 
-    result = [left_products[i] * right_products[i] for i in range(0, len(nums))]
+    result = [left_products[i] * right_products[i] for i in range(len(nums))]
+    return result
+
+
+# Calculate the left product and right index in the fly
+# Complexity: T-O(n), S-O(n) (the output array does not count as extra space for space complexity analysis)
+def product_of_array_except_self2(nums: list[int]) -> list[int]:
+    result = [1] * len(nums)
+
+    left = 1
+    for i in range(len(nums)):
+        result[i] = left
+        left = left * nums[i]
+
+    right = 1
+    for i in range(len(nums) - 1, -1, -1):
+        result[i] = result[i] * right
+        right = right * nums[i]
+
     return result
--- src/array_hash/product_of_array_except_self_test.py
+++ src/array_hash/product_of_array_except_self_test.py
@@ -1,13 +1,32 @@
-def test_no_zero_input():
-    from product_of_array_except_self import product_of_array_except_self 
 
-    assert product_of_array_except_self([1, 2, 3, 4]) == [24, 12, 8, 6]
-    assert product_of_array_except_self([1, 2, 3, 4, 5]) == [120, 60, 40, 30, 24]
-    assert product_of_array_except_self([1, 2, 3, 4, 5, 6]) == [720, 360, 240, 180, 144, 120]
 
-def test_zero_input():
-    from product_of_array_except_self import product_of_array_except_self
+def test_product_of_array_except_self2():
+    from product_of_array_except_self import product_of_array_except_self2
 
-    assert product_of_array_except_self([0, 2, 3, 4]) == [24, 0, 0, 0]
-    assert product_of_array_except_self([1, 2, 3, 4, 5, 0]) == [0, 0, 0, 0, 0, 120]
-    assert product_of_array_except_self([0, 0, 3, 4, 5, 6]) == [720, 720, 0, 0, 0, 0]
+    assert product_of_array_except_self2([1, 2, 3, 4]) == [24, 12, 8, 6]
+    assert product_of_array_except_self2([1, 2, 3, 4, 5]) == [
+        120, 60, 40, 30, 24]
+    assert product_of_array_except_self2([1, 2, 3, 4, 5, 6]) == [
+        720, 360, 240, 180, 144, 120]
+
+    assert product_of_array_except_self2([0, 2, 3, 4]) == [24, 0, 0, 0]
+    assert product_of_array_except_self2([1, 2, 3, 4, 5, 0]) == [
+        0, 0, 0, 0, 0, 120]
+    assert product_of_array_except_self2([0, 0, 3, 4, 5, 6]) == [
+        0, 0, 0, 0, 0, 0]
+
+
+def test_product_of_array_except_self():
+    from product_of_array_except_self import product_of_array_except_self2
+
+    assert product_of_array_except_self2([1, 2, 3, 4]) == [24, 12, 8, 6]
+    assert product_of_array_except_self2([1, 2, 3, 4, 5]) == [
+        120, 60, 40, 30, 24]
+    assert product_of_array_except_self2([1, 2, 3, 4, 5, 6]) == [
+        720, 360, 240, 180, 144, 120]
+
+    assert product_of_array_except_self2([0, 2, 3, 4]) == [24, 0, 0, 0]
+    assert product_of_array_except_self2([1, 2, 3, 4, 5, 0]) == [
+        0, 0, 0, 0, 0, 120]
+    assert product_of_array_except_self2([0, 0, 3, 4, 5, 6]) == [
+        0, 0, 0, 0, 0, 0]
