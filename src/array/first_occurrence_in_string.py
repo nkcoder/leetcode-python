6a2cd2c2c658231dfae41c4dae43f5a965c0ae5b
daniel.guo <daniel.guo@vivalabs.com.au>
2024-12-09 18:48:53 +1100

    Update comment: first occurrence in string

--- src/array/first_occurrence_in_string.py
+++ src/array/first_occurrence_in_string.py
@@ -1,3 +1,7 @@
+# Easy: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description
+# Use substring, T-O(n), S-O(1)
+
+
 def first_occurrence_in_string(haystack: str, needle: str) -> int:
     for i in range(0, len(haystack)):
         if haystack[i : i + len(needle)] == needle:
